d75748248db9b37b18821f262bce69a3bdd2140e
KimMatt <mk1995x@gmail.com>
2022-05-26 00:56:18 +0100

    format

--- Pigeon/auto_exposure.py
+++ Pigeon/auto_exposure.py
@@ -10,7 +10,9 @@ TARGET_LUM = math.floor(MIN_LUM + (MAX_LUM - MIN_LUM)/2)
 ALPHA = 0.25
 BOX_HEIGHT = 360
 BOX_WIDTH = 640
-WEIGHT_MATRIX = np.array([[1 if i in range (int(1920/2 - BOX_WIDTH/2), int(1920/2 + BOX_WIDTH/2)) else 0 for i in range(1920)] if j in range(int(1080/2 - BOX_HEIGHT/2), int(1080/2 + BOX_HEIGHT/2)) else [0 for i in range(1920)] for j in range(1080)])
+CAMERA_WIDTH = 1920
+CAMERA_HEIGHT = 1080
+WEIGHT_MATRIX = np.array([[1 if i in range (int(CAMERA_WIDTH/2 - BOX_WIDTH/2), int(CAMERA_WIDTH/2 + BOX_WIDTH/2)) else 0 for i in range(CAMERA_WIDTH)] if j in range(int(CAMERA_HEIGHT/2 - BOX_HEIGHT/2), int(CAMERA_HEIGHT/2 + BOX_HEIGHT/2)) else [0 for i in range(CAMERA_WIDTH)] for j in range(CAMERA_HEIGHT)])
 NORMALIZATION_FACTOR = 1/(np.max(np.max(WEIGHT_MATRIX)))
 
 def calculate_avg_box_lum(img):
@@ -22,9 +24,9 @@ def calculate_avg_box_lum(img):
     return np.sum(weighted_box_lum) / (BOX_WIDTH * BOX_HEIGHT)
 
 with PiCamera() as camera:
-    stream = io.BytesIO()
-    camera.resolution = (1920, 1080)
-    rawCapture = PiRGBArray(camera, size=(1920,1080))
+    #stream = io.BytesIO()
+    camera.resolution = (CAMERA_WIDTH, CAMERA_HEIGHT)
+    rawCapture = PiRGBArray(camera, size=(CAMERA_WIDTH, CAMERA_HEIGHT))
 
     for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=True):
         img = np.array(frame.array)
@@ -32,4 +34,8 @@ with PiCamera() as camera:
         if avg_box_lum > MIN_LUM or avg_box_lum > MAX_LUM:
             exposure_step_size = ALPHA * (TARGET_LUM - avg_box_lum)
             target_exposure = camera.get_current_exposure() + exposure_step_size
+            #rawCapture.truncate(0)
             camera.set_exposure(target_exposure)
+            print("target exposure: " + str(target_exposure)) 
+            rawCapture.truncate(0)
+            rawCapture.seek(0)
